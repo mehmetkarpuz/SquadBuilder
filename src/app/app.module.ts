e8db1843f3cdb8f407d0c9756d31984727ded6a8
mehmetkarpuz <memetkarpuz@gmail.com>
2018-11-12 15:43:07 +0300

    Basic template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {DragDropModule } from '@angular/cdk/drag-drop';
+import { FormsModule } from '@angular/forms';
+
+import {UsedAngualarMaterial} from './angularmaterial.modoule';
+
 
 import { AppComponent } from './app.component';
+import { PlayerComponent } from './player/player.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UsedAngualarMaterial,
+    DragDropModule,
+    FormsModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
